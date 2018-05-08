3fcda1eb6ff00c43def2dd2756bdbc66261a924a
Markus <markusmattsson90@gmail.com>
2018-05-08 13:16:34 +0200

    Add static html and css for quiz comp

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,8 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './quiz.css';
 
 function Quiz(props) {
   return (
-    <h1>Quiz</h1>
+    <div id="quizContainer">
+      <h3>What is the answer to this question?</h3>
+      <ul className="quizInfo">
+        <li>Timer: 60</li>
+        <li>Totalpoints: 100</li> 
+        <li>Combo: 3 x</li>
+      </ul>
+      <div className="altDiv">
+        <input type="radio" name="alts" id="alt1"/>
+        <label htmlFor="alt1">alt1</label>
+        <input type="radio" name="alts" id="alt2"/>
+        <label htmlFor="alt2">alt2</label>
+        <input type="radio" name="alts" id="alt3"/>
+        <label htmlFor="alt3">alt3</label>
+        <input type="radio" name="alts" id="alt4"/>
+        <label htmlFor="alt4">alt4</label>
+      </div>
+      <button id="cancelBtn" className="btn">X</button>
+      <input  className="btn" type="submit" value="Submit"/>
+    </div>
+
+    //on submit or no more time, highlight correct answer
   )
 }
 
