c8ec1725b3c13aaf4ebf7cfdc59f0b6583e7cf26
Albin Engman <albin.a.engman@gmail.com>
2018-05-08 16:06:03 +0200

    Added highscore css and html, put it in a component and can be rendered

---
+++ src/components/highscoreEntries.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+function HighscoreEntries(props) {
+  return (
+      <div className = "highContainer"> 90s Rock Highscores
+        <div className = "highScoreOne">
+          <img src="https://i.pinimg.com/736x/5b/91/00/5b91000392c81eb72e26ba7253e3d502--manga-anime-anime-art.jpg"/>
+          <div className = "nameDiv">
+          Okami Goodgame
+          </div>
+          <div className = "pointDiv">
+          points: 2081
+          </div>
+        </div>
+        <div className = "highScoreOne">
+          <img src="https://i.pinimg.com/736x/5b/91/00/5b91000392c81eb72e26ba7253e3d502--manga-anime-anime-art.jpg"/>
+          <div className = "nameDiv">
+          Okami Goodgame
+          </div>
+          <div className = "pointDiv">
+          points: 2081
+          </div>
+        </div>
+        <div className = "highScoreOne">
+          <img src="https://i.pinimg.com/736x/5b/91/00/5b91000392c81eb72e26ba7253e3d502--manga-anime-anime-art.jpg"/>
+          <div className = "nameDiv">
+          Okami Goodgame
+          </div>
+          <div className = "pointDiv">
+          points: 2081
+          </div>
+        </div>
+        <div className = "selfDiv">
+          Own: 200
+        </div>
+      </div>
+  )
+}
+
+export default HighscoreEntries;
