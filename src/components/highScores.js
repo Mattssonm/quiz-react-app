83fc5b8dad85d6783580216bb6abde1fcce0f64c
Albin Engman <albin.a.engman@gmail.com>
2018-05-10 11:49:22 +0200

    Fixed firebase highscores - image not implemented yet however

--- src/components/highScores.js
+++ src/components/highScores.js
@@ -8,22 +8,38 @@ class HighScores extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+      highscore90: 0,
+      highscore80: 0,
+      highscore70: 0,
 
     };
   }
-  componentDidMount(){
-    firebaseDB.ref('quiz').once('value', function(snapshot){
-      console.log(snapshot.val());
+  componentDidMount() {
+    firebaseDB.ref('quiz').once('value', (snapshot) => {
+      var snapVal = snapshot.val();
+      this.setState({ highscore90: snapVal.rock90.highscore, highscore80: snapVal.rock80.highscore, highscore70: snapVal.rock70.highscore})
     })
   }
   render (){
+    let highscoreElementOne;
+    let highscoreElementTwo;
+    let highscoreElementThree
+    if (this.state.highscore90 !== 0){
+      highscoreElementOne = <HighscoreEntries highscores={this.state.highscore90}/>
+      highscoreElementTwo = <HighscoreEntries highscores={this.state.highscore80}/>
+      highscoreElementThree = <HighscoreEntries highscores={this.state.highscore70}/>
+    } else {
+      highscoreElementOne = <div> Error </div>
+      highscoreElementTwo = <div> Error </div>
+      highscoreElementThree = <div> Error </div>
+    }
     return (
       <div>
         <h1> High Scores</h1>
         <div className = "fullHighContainer">
-          <HighscoreEntries/>
-          <HighscoreEntries/>
-          <HighscoreEntries/>
+          {highscoreElementOne}
+          {highscoreElementTwo}
+          {highscoreElementThree}
         </div>
       </div>
     )
--- src/components/highscoreEntries.js
+++ src/components/highscoreEntries.js
@@ -1,40 +1,45 @@
 import React, { Component } from 'react';
 
-function HighscoreEntries(props) {
-  return (
-      <div className = "highContainer"> 90s Rock Highscores
-        <div className = "highScoreOne">
-          <img src="https://i.pinimg.com/736x/5b/91/00/5b91000392c81eb72e26ba7253e3d502--manga-anime-anime-art.jpg" alt=""/>
-          <div className = "nameDiv">
-          Okami Goodgame
+class HighscoreEntries extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    console.log(this.props.highscores)
+    return (
+        <div className = "highContainer"> 90s Rock Highscores
+          <div className = "highScoreOne">
+            <img src="https://i.pinimg.com/736x/5b/91/00/5b91000392c81eb72e26ba7253e3d502--manga-anime-anime-art.jpg" alt=""/>
+            <div className = "nameDiv">
+              {this.props.highscores.first.name}
+            </div>
+            <div className = "pointDiv">
+            points: {this.props.highscores.first.points}
+            </div>
           </div>
-          <div className = "pointDiv">
-          points: 2081
+          <div className = "highScoreOne">
+            <img src="https://i.pinimg.com/736x/5b/91/00/5b91000392c81eb72e26ba7253e3d502--manga-anime-anime-art.jpg" alt=""/>
+            <div className = "nameDiv">
+              {this.props.highscores.second.name}
+            </div>
+            <div className = "pointDiv">
+            points: {this.props.highscores.second.points}
+            </div>
+          </div>
+          <div className = "highScoreOne">
+            <img src="https://i.pinimg.com/736x/5b/91/00/5b91000392c81eb72e26ba7253e3d502--manga-anime-anime-art.jpg" alt=""/>
+            <div className = "nameDiv">
+              {this.props.highscores.third.name}
+            </div>
+            <div className = "pointDiv">
+            points: {this.props.highscores.third.points}
+            </div>
+          </div>
+          <div className = "selfDiv">
+            Own: 200
           </div>
         </div>
-        <div className = "highScoreOne">
-          <img src="https://i.pinimg.com/736x/5b/91/00/5b91000392c81eb72e26ba7253e3d502--manga-anime-anime-art.jpg" alt=""/>
-          <div className = "nameDiv">
-          Okami Goodgame
-          </div>
-          <div className = "pointDiv">
-          points: 2081
-          </div>
-        </div>
-        <div className = "highScoreOne">
-          <img src="https://i.pinimg.com/736x/5b/91/00/5b91000392c81eb72e26ba7253e3d502--manga-anime-anime-art.jpg" alt=""/>
-          <div className = "nameDiv">
-          Okami Goodgame
-          </div>
-          <div className = "pointDiv">
-          points: 2081
-          </div>
-        </div>
-        <div className = "selfDiv">
-          Own: 200
-        </div>
-      </div>
-  )
+    )}
 }
 
 export default HighscoreEntries;
